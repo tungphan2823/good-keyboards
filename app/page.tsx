c1d03a5da639381ffd93dc4bd3441859c9d453ea
Tung Phan <thanhtungphanvu@gmail.com>
2024-05-13 03:02:31 +0300

    adding sqlite

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,8 @@ import keyboard from "../public/keyboard/testkeyboard.jpg";
 import keyboard1 from "../public/keyboard/switch.gif";
 import SouthIcon from "@mui/icons-material/South";
 import NavBar from "@/components/navBar";
+import AutoScrolling from "@/components/autoScroll";
+import kb81 from "../public/keyboard/kb81.jpg";
 export default function Home() {
   return (
     <main>
@@ -20,9 +22,9 @@ export default function Home() {
             <h1>— find your next </h1>
             <h1>favorite keyboard.</h1>
           </div>
-          <div className="p-8">
-            — Keyboard recommendations from the most <br></br> successful and interesting
-            company in the world.
+          <div className="p-6">
+            — Keyboard recommendations from the most <br></br> successful and
+            interesting company in the world.
           </div>
         </div>
         <div className="w-1/2 text-black flex justify-center  items-end  font-Freeman p-8 text-2xl">
@@ -32,10 +34,65 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <div>Hellodwdwdd</div>
-      <div>Hellodwdwdd</div>
-      <div>Hellodwdwdd</div>
-     
+      <div className="border-b-2 border-black p-8 text-black ">
+        <div className="text-2xl font-Freeman">Famous Brand</div>
+        <AutoScrolling scrollType="tool" />
+      </div>
+      <div className="p-8">
+        <h1 className="font-Freeman text-2xl text-black">
+          Top recommended keyboards
+        </h1>
+        <div className="grid grid-cols-4 gap-4">
+          <div className="p-8 w-96 ">
+            <div>
+              <Image className="rounded-3xl  " src={kb81} alt=""></Image>
+              <h1 className="text-black p-4 ">
+                Keyboard 81 Pro QMK/VIA Wireless Custom Mechanical Keyboard
+              </h1>
+            </div>
+          </div>
+          <div className="p-8 w-96 ">
+            <div>
+              <Image className="rounded-3xl  " src={kb81} alt=""></Image>
+              <h1 className="text-black p-4 ">
+                Keyboard 81 Pro QMK/VIA Wireless Custom Mechanical Keyboard
+              </h1>
+            </div>
+          </div>
+          <div className="p-8 w-96 ">
+            <div>
+              <Image className="rounded-3xl  " src={kb81} alt=""></Image>
+              <h1 className="text-black p-4 ">
+                Keyboard 81 Pro QMK/VIA Wireless Custom Mechanical Keyboard
+              </h1>
+            </div>
+          </div>
+          <div className="p-8 w-96 ">
+            <div>
+              <Image className="rounded-3xl  " src={kb81} alt=""></Image>
+              <h1 className="text-black p-4 ">
+                Keyboard 81 Pro QMK/VIA Wireless Custom Mechanical Keyboard
+              </h1>
+            </div>
+          </div>
+          <div className="p-8 w-96 ">
+            <div>
+              <Image className="rounded-3xl  " src={kb81} alt=""></Image>
+              <h1 className="text-black p-4 ">
+                Keyboard 81 Pro QMK/VIA Wireless Custom Mechanical Keyboard
+              </h1>
+            </div>
+          </div>
+          <div className="p-8 w-96 ">
+            <div>
+              <Image className="rounded-3xl  " src={kb81} alt=""></Image>
+              <h1 className="text-black p-4 ">
+                Keyboard 81 Pro QMK/VIA Wireless Custom Mechanical Keyboard
+              </h1>
+            </div>
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
---
+++ components/autoScroll.tsx
@@ -0,0 +1,49 @@
+import Marquee from "react-fast-marquee";
+import AcUnitIcon from "@mui/icons-material/AcUnit";
+type ScrollingProps = {
+  scrollType: string;
+};
+import Image from "next/image";
+import AutoAwesomeIcon from "@mui/icons-material/AutoAwesome";
+import akko from "../public/company/akko.png";
+import divini from "../public/company/divini.png";
+import logitech from "../public/company/logitech.png";
+import RK from "../public/company/RK.png";
+export default function AutoScrolling({ scrollType }: ScrollingProps) {
+  return (
+    <div className="">
+      <div className="w-[50%] flex flex-col pt-14 "></div>
+
+      {scrollType === "landing" && (
+        <>
+          <Marquee autoFill pauseOnClick>
+            <div className=" m-1 rounded-xl flex space-x-5 p-16  font-bold text-xs cursor-pointer items-center gap-1">
+              <div className=" animate-spin-slow	">
+                <AcUnitIcon />
+              </div>
+              /SOFTWARE DEVELOPER /WEB DEVELOPER /STUDENT
+            </div>
+          </Marquee>
+        </>
+      )}
+      {scrollType === "tool" && (
+        <>
+          <Marquee autoFill pauseOnClick>
+            <div className=" m-1 rounded-xl flex space-x-5 p-16  font-bold text-xs cursor-pointer items-center gap-1 max-w-48">
+              <Image src={akko} alt="js"></Image>
+            </div>
+            <div className=" m-1 rounded-xl flex space-x-5 p-16  font-bold text-xs cursor-pointer items-center gap-1 max-w-48">
+              <Image src={divini} alt="ts"></Image>
+            </div>
+            <div className=" m-1 rounded-xl flex space-x-5 p-16  font-bold text-xs cursor-pointer items-center gap-1 max-w-48">
+              <Image src={logitech} alt="rs"></Image>
+            </div>
+            <div className=" m-1 rounded-xl flex space-x-5 p-16  font-bold text-xs cursor-pointer items-center gap-1 max-w-48">
+              <Image src={RK} alt="css"></Image>
+            </div>
+          </Marquee>
+        </>
+      )}
+    </div>
+  );
+}
