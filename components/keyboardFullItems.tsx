54389fcb37820b6351786ed6143b5bfc7fd3b8e9
Tung Phan <thanhtungphanvu@gmail.com>
2024-05-16 04:10:13 +0300

    update

--- components/keyboardFullItems.tsx
+++ components/keyboardFullItems.tsx
@@ -35,111 +35,102 @@ export default function KeyboardFullItems({
     }
   }, [searchParams]);
   return (
-    <>
-      <Suspense fallback={<div>Loading...</div>}>
-        <div>
-          <h1 className="font-Freeman text-2xl lg:text-4xl text-black p-10">
-            All recommended keyboards
-          </h1>
+    <Suspense fallback={<div>Loading...</div>}>
+      <div>
+        <h1 className="font-Freeman text-2xl lg:text-4xl text-black p-10">
+          All recommended keyboards
+        </h1>
 
-          <label className="p-10 text-black font-Freeman">Brand</label>
-          <p className="p-10 text-black font-Freeman">Choose filter</p>
-          <div className="flex text-black gap-4 p-10 overflow-x-scroll lg:overflow-x-hidden">
-            <div
-              className="p-4 bg-slate-50 rounded-full cursor-pointer hover:border-2 hover:border-black "
-              onClick={() => {
-                setFilter("NuPhy");
-              }}
-            >
-              <Link href={`?filter=${isFilter ? "normal " : "NuPhy"}`}>
-                {" "}
-                NuPhy
-              </Link>
-            </div>
-            <div
-              className="p-4 bg-slate-50 rounded-full cursor-pointer  hover:border-2 hover:border-black"
-              onClick={() => setFilter("akko")}
-            >
-              <Link href={`?filter=${isFilter ? "normal " : "akko"}`}>
-                {" "}
-                Akko
-              </Link>
-            </div>
-            <div
-              className="p-4 bg-slate-50 rounded-full cursor-pointer hover:border-2 hover:border-black"
-              onClick={() => setFilter("keychron")}
-            >
-              <Link href={`?filter=${isFilter ? "normal " : "keychron"}`}>
-                {" "}
-                Keychron
-              </Link>
-            </div>
-            <div
-              className="p-4 bg-slate-50 rounded-full cursor-pointer hover:border-2 hover:border-black"
-              onClick={() => setFilter("Epomaker")}
-            >
-              <Link href={`?filter=${isFilter ? "normal " : "Epomaker"}`}>
-                {" "}
-                Epomaker
-              </Link>
-            </div>
+        <label className="p-10 text-black font-Freeman">Brand</label>
+        <p className="p-10 text-black font-Freeman">Choose filter</p>
+        <div className="flex text-black gap-4 p-10 overflow-x-scroll lg:overflow-x-hidden">
+          <div
+            className="p-4 bg-slate-50 rounded-full cursor-pointer hover:border-2 hover:border-black "
+            onClick={() => {
+              setFilter("NuPhy");
+            }}
+          >
+            <Link href={`?filter=${isFilter ? "normal " : "NuPhy"}`}>
+              {" "}
+              NuPhy
+            </Link>
+          </div>
+          <div
+            className="p-4 bg-slate-50 rounded-full cursor-pointer  hover:border-2 hover:border-black"
+            onClick={() => setFilter("akko")}
+          >
+            <Link href={`?filter=${isFilter ? "normal " : "akko"}`}> Akko</Link>
+          </div>
+          <div
+            className="p-4 bg-slate-50 rounded-full cursor-pointer hover:border-2 hover:border-black"
+            onClick={() => setFilter("keychron")}
+          >
+            <Link href={`?filter=${isFilter ? "normal " : "keychron"}`}>
+              {" "}
+              Keychron
+            </Link>
+          </div>
+          <div
+            className="p-4 bg-slate-50 rounded-full cursor-pointer hover:border-2 hover:border-black"
+            onClick={() => setFilter("Epomaker")}
+          >
+            <Link href={`?filter=${isFilter ? "normal " : "Epomaker"}`}>
+              {" "}
+              Epomaker
+            </Link>
           </div>
         </div>
-        <div className=" lg:grid lg:grid-cols-4 lg:overflow-x-hidden   border-b-2 border-gray-50">
-          {!isFilter &&
-            keyboard.map((keyboard, index) => (
-              <div className="p-8   object-cover flex-initial " key={index}>
-                <Link href={`/keyboards/${keyboard.slug}`}>
-                  <div className="">
-                    <div className=" relative h-80 w-80 lg:w-96 lg:h-96">
-                      {" "}
-                      <Image
-                        fill
-                        className="rounded-3xl "
-                        src={JSON.parse(keyboard.image)[0]}
-                        alt={keyboard.title}
-                      />
-                    </div>
-                    <div className="text-black p-4">
-                      {" "}
-                      <h1 className="font-bold">{keyboard.title}</h1>
-                      <p className="text-sm text-gray-600">
-                        {keyboard.summary}
-                      </p>
-                      <p className=" "> €{keyboard.price}</p>
-                    </div>
+      </div>
+      <div className=" lg:grid lg:grid-cols-4 lg:overflow-x-hidden   border-b-2 border-gray-50">
+        {!isFilter &&
+          keyboard.map((keyboard, index) => (
+            <div className="p-8   object-cover flex-initial " key={index}>
+              <Link href={`/keyboards/${keyboard.slug}`}>
+                <div className="">
+                  <div className=" relative h-80 w-80 lg:w-96 lg:h-96">
+                    {" "}
+                    <Image
+                      fill
+                      className="rounded-3xl "
+                      src={JSON.parse(keyboard.image)[0]}
+                      alt={keyboard.title}
+                    />
                   </div>
-                </Link>
-              </div>
-            ))}
-          {isFilter &&
-            filteredKeyboards.map((keyboard, index) => (
-              <div className="p-8   object-cover flex-initial " key={index}>
-                <Link href={`/keyboards/${keyboard.slug}`}>
-                  <div className="">
-                    <div className=" relative h-80 w-80 lg:w-96 lg:h-96">
-                      {" "}
-                      <Image
-                        fill
-                        className="rounded-3xl "
-                        src={JSON.parse(keyboard.image)[0]}
-                        alt={keyboard.title}
-                      />
-                    </div>
-                    <div className="text-black p-4">
-                      {" "}
-                      <h1 className="font-bold">{keyboard.title}</h1>
-                      <p className="text-sm text-gray-600">
-                        {keyboard.summary}
-                      </p>
-                      <p className=" "> €{keyboard.price}</p>
-                    </div>
+                  <div className="text-black p-4">
+                    {" "}
+                    <h1 className="font-bold">{keyboard.title}</h1>
+                    <p className="text-sm text-gray-600">{keyboard.summary}</p>
+                    <p className=" "> €{keyboard.price}</p>
                   </div>
-                </Link>
-              </div>
-            ))}
-        </div>
-      </Suspense>
-    </>
+                </div>
+              </Link>
+            </div>
+          ))}
+        {isFilter &&
+          filteredKeyboards.map((keyboard, index) => (
+            <div className="p-8   object-cover flex-initial " key={index}>
+              <Link href={`/keyboards/${keyboard.slug}`}>
+                <div className="">
+                  <div className=" relative h-80 w-80 lg:w-96 lg:h-96">
+                    {" "}
+                    <Image
+                      fill
+                      className="rounded-3xl "
+                      src={JSON.parse(keyboard.image)[0]}
+                      alt={keyboard.title}
+                    />
+                  </div>
+                  <div className="text-black p-4">
+                    {" "}
+                    <h1 className="font-bold">{keyboard.title}</h1>
+                    <p className="text-sm text-gray-600">{keyboard.summary}</p>
+                    <p className=" "> €{keyboard.price}</p>
+                  </div>
+                </div>
+              </Link>
+            </div>
+          ))}
+      </div>
+    </Suspense>
   );
 }
